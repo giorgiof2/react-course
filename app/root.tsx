8445ced8503856d5b203f3dc6aa40871fa388594
Giorgio Tognoli <giorgio@f2.net>
2022-11-28 11:44:07 +0100

    prettierizzato

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,23 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node'
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import type { LinksFunction, MetaFunction } from "@remix-run/node"
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react"
 
-import styles from './styles/app.css'
+import styles from "./styles/app.css"
 
 export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: styles }]
+  return [{ rel: "stylesheet", href: styles }]
 }
 
 export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
 })
 
 export default function App() {
